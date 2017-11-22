dd3b34a00e3eacbd90083bb36cf92897fd2ba6dd
Nando Septian Husni <septianhusni93@gmail.com>
2017-11-22 14:07:38 +0700

    initial commit

---
+++ app/src/main/java/id/co/imastudio/libawaydevsummit/Utama.java
@@ -0,0 +1,13 @@
+package id.co.imastudio.libawaydevsummit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Utama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utama);
+    }
+}
